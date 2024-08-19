c5cce5821d745a308fcb86ebcc8badedf3aa7f0f
ashenjay <90669153+ashenjay@users.noreply.github.com>
2024-08-19 14:28:19 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3testashenjay" # Replace with your actual S3 bucket name
+    bucket = "firstekswithtf1" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
